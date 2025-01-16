bb71f950c2a7eb67690113b86fd688a6d22c52be
saylees9312 <saylees9312@naver.com>
2025-01-16 17:55:26 +0900

    반응형 수정중2

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ const snsSlider = new Swiper(".sns-slider", {
     autoplay: true,
     speed: 1000,
     centeredSlides: true, // 활성화된 슬라이드를 가운데로
-    slidesPerView: 8,
+    slidesPerView: 1,
     spaceBetween: 20,
 
     pagination: {
@@ -14,6 +14,26 @@ const snsSlider = new Swiper(".sns-slider", {
     navigation: {
         nextEl: ".sw-btn-next",
         prevEl: ".sw-btn-prev",
+    },
+
+    breakpoints: {
+        // when window width is >= 320px
+        380: {
+            slidesPerView: 1.5,
+        },
+        // when window width is >= 560px
+        560: {
+            slidesPerView: 3,
+        },
+
+        // when window width is >= 800px
+        800: {
+            slidesPerView: 6,
+        },
+        // when window width is >= 1200px
+        1200: {
+            slidesPerView: 8,
+        },
     },
 });
 
